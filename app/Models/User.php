99d690433c35ebbdc49e468dfeed388ba218314c
Mitesh Viras <virashmitesh@gmail.com>
2025-03-08 22:29:58 +0530

    add AdminUserCommand to create admin users; update User model to include email_verified_at

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements FilamentUser, MustVerifyEmail
         'name',
         'email',
         'password',
+        'email_verified_at',
         'is_active' => 'boolean',
         'is_admin' => 'boolean',
     ];
